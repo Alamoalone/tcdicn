fd24a4cdb157751540e32a8d65185a299558246a
alamoalone <Alamoalone@users.noreply.github.com>
2023-11-11 14:16:29 +0000

    Update drone.py
    update for tcdicn v2.0

--- drone.py
+++ drone.py
@@ -1,8 +1,11 @@
 import asyncio
 import random
+import logging
 from tcdicn import Server
 
-# Define the drone
+# Configure basic logging
+logging.basicConfig(level=logging.INFO)
+
 class Drone:
     def __init__(self, server, drone_id):
         self.server = server
@@ -14,31 +17,41 @@ class Drone:
 
     async def update_sensors(self):
         while True:
-            # Simulate sensor reading changes
-            self.position = (self.position[0] + random.uniform(-0.1, 0.1),
-                             self.position[1] + random.uniform(-0.1, 0.1))
-            self.temperature += random.uniform(-0.5, 0.5)
-            self.battery -= self.power_usage / 100
+            try:
+                # Simulate sensor reading changes
+                self.position = (self.position[0] + random.uniform(-0.1, 0.1),
+                                 self.position[1] + random.uniform(-0.1, 0.1))
+                self.temperature += random.uniform(-0.5, 0.5)
+                self.battery -= self.power_usage / 100
 
-            # Publish sensor readings
-            await self.server.set(f"{self.drone_id}-position", str(self.position))
-            await self.server.set(f"{self.drone_id}-temperature", str(self.temperature))
-            await self.server.set(f"{self.drone_id}-battery", str(self.battery))
+                # Publish sensor readings
+                await self.server.set(f"{self.drone_id}-position", str(self.position))
+                await self.server.set(f"{self.drone_id}-temperature", str(self.temperature))
+                await self.server.set(f"{self.drone_id}-battery", str(self.battery))
 
-            # Wait a bit before next update
-            await asyncio.sleep(5)
+                # Reduce update frequency when battery is low
+                if self.battery < 20:
+                    await asyncio.sleep(10)
+                else:
+                    await asyncio.sleep(5)
+            except Exception as e:
+                logging.error(f"Error updating sensors: {e}")
+                await asyncio.sleep(5)  # Wait before retrying in case of error
 
     async def subscribe_to_commands(self):
         while True:
-            # Subscribe to commands for this drone
-            command = await self.server.get(f"command-{self.drone_id}")
-            if command:
-                # Process command
-                print(f"Received command for {self.drone_id}: {command}")
-                # Implement command handling logic here
+            try:
+                # Subscribe to commands for this drone
+                command = await self.server.get(f"command-{self.drone_id}")
+                if command:
+                    logging.info(f"Received command for {self.drone_id}: {command}")
+                    # Implement command handling logic here
+                    # ...
 
-            # Wait a bit before checking for new commands
-            await asyncio.sleep(1)
+                await asyncio.sleep(1)
+            except Exception as e:
+                logging.error(f"Error receiving command: {e}")
+                await asyncio.sleep(1)  # Wait before retrying in case of error
 
     async def run(self):
         # Start updating sensors and subscribing to commands
